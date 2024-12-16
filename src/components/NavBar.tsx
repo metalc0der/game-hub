85e1019f7213b7264e7b5ae4b1c8607e66ad6a9e
Metalcoder <yunior.rdguez@gmail.com>
2024-12-16 15:15:24 -0500

    Implement dark mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from '@chakra-ui/react'
 import logo from '../assets/Logo/logo.webp'
+import { ColorModeButton } from './ui/color-mode'
 
 const NavBar = () => {
   return (
     <HStack>
         <Image src={ logo } alt="logo" boxSize="60px"/>
         <Text>Navegation Bar</Text>
+        <ColorModeButton /> 
     </HStack>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { ColorModeProvider } from "./components/ui/color-mode"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider>
-      <App />
+      <ColorModeProvider>
+        <App />
+      </ColorModeProvider>
     </Provider>
   </React.StrictMode>,
 )
