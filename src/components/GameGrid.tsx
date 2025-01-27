4a38637064f9acba7eae76e276850069b2640351
Metalcoder <yunior.rdguez@gmail.com>
2025-01-27 16:22:33 -0500

    implementing infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,11 @@
 import useGames from '@/hooks/useGames';
-import { Box, Button, SimpleGrid, Text } from '@chakra-ui/react';
+import { SimpleGrid, Spinner, Text } from '@chakra-ui/react';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
 import GameCardContainer from './GameCardContainer';
 import { GameQuery } from '@/App';
 import React from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 interface Props {
     gameQuery: GameQuery
@@ -17,9 +18,11 @@ const GameGrid = ({ gameQuery }: Props) => {
 
     if (error) return <Text>{error.message}</Text>
 
+    const fetchedGamesCount = data?.pages.reduce((acc, page) => acc + page.results.length, 0) || 0;
+
   return (
-    <Box padding='10px'>
-        <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} gap={6}>
+    <InfiniteScroll dataLength={fetchedGamesCount} hasMore={!!hasNextPage} next={() => fetchNextPage()} loader={<Spinner />}>
+        <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} gap={6} padding='10px'>
             {isLoading && skeletons.map((skeleton) => (
                 <GameCardContainer key={skeleton}>
                     <GameCardSkeleton />
@@ -35,8 +38,7 @@ const GameGrid = ({ gameQuery }: Props) => {
                 </React.Fragment>
             )}
         </SimpleGrid>
-        {hasNextPage && <Button onClick={() => fetchNextPage()} marginY={5}>{isFetchingNextPage ? 'Loading...' : 'Load More'}</Button>}
-    </Box>
+    </InfiniteScroll>
   )
 }
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -28,6 +28,7 @@ const useGames = (gameQuery: GameQuery) => useInfiniteQuery<FetchResponse<Game>,
     getNextPageParam: (lastPage, allPages) => {
         return lastPage.next ? allPages.length + 1 : undefined;
     },  
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
 });
 
 export default useGames;
