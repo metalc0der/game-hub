425a5fdbea97a211bd848d3315b731b91b21b331
Metalcoder <yunior.rdguez@gmail.com>
2025-02-20 14:53:01 -0500

    Building game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
 import { Platform } from "@/entities/Platform";
+import { Genre } from "./Genre";
+import { Publisher } from "./Publisher";
 
 
 export interface Game {
@@ -10,4 +12,6 @@ export interface Game {
     rating_top: number;
     slug: string;
     description_raw: string;
+    genres: Genre[];
+    publishers: Publisher[];
 }
